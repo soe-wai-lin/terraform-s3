3bf8dac259d817b1385bb7826b8e5e7b6f874ea3
soewailin <09soewailynn@gmail.com>
2024-06-13 12:49:21 +0700

    modify outputs.tf

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,11 +1,11 @@
 
 output "aws_s3_arn" {
-    value = aws_s3_bucket.terraform_state.arn
+    value = resource.aws_s3_bucket.terraform_state.arn
     description = "arn of s3 bucket"
 }
 
 output "dynamodb_table_name" { 
-    value = aws_dynamodb_table.terraform_locks.name
+    value = resource.aws_dynamodb_table.terraform_locks.name
     description = "name of dynamodb table"
 }
 
